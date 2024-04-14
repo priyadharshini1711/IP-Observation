61765afc6ed229d963a74315a840259d3718c5ac
priyadharshini1711 <priyadharshini170898@gmail.com>
2024-04-14 23:56:46 +0530

    php commit

---
+++ php/005.datatype.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 5;
+var_dump($x);
+echo $x;
+
+
+$x = "Hello world!";
+$y = 'Hello world!';
+
+var_dump($x);
+echo "<br>";
+var_dump($y);
+
+$x = 5985;
+var_dump($x);
+
+$x = 10.365;
+var_dump($x);
+
+$x = true;
+var_dump($x);
+
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+
+class Car {
+    public $color;
+    public $model;
+    public function __construct($color, $model) {
+      $this->color = $color;
+      $this->model = $model;
+    }
+    public function message() {
+      return "My car is a " . $this->color . " " . $this->model . "!";
+    }
+  }
+  
+  $myCar = new Car("red", "Volvo");
+  var_dump($myCar);
+
+
+$x = "Hello world!";
+$x = null;
+var_dump($x);
+
+$x = 5;
+var_dump($x);
+
+$x = "Hello";
+var_dump($x);
+
+$x = 5;
+$x = (string) $x;
+var_dump($x);
+
+?>
+</body>
+</html>
---
+++ php/010.operators.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 10;  
+$y = 6;
+
+echo $x + $y;
+echo "<br>";
+echo $x - $y;
+echo "<br>";
+echo $x * $y;
+echo "<br>";
+echo $x / $y;
+echo "<br>";
+echo $x % $y;
+echo "<br>";
+echo $x ** $y;
+?>  
+
+</body>a
+</html>
